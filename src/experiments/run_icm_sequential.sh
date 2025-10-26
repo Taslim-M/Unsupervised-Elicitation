6318d2e6cf8f6604044ad305c6a001d1a6db6237
Taslim <taslimmahbub@hotmail.com>
2025-10-26 00:38:52 -0400

    Update run_icm_sequential.sh

--- src/experiments/run_icm_sequential.sh
+++ src/experiments/run_icm_sequential.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Array of all JSON file names
-files=(
-Brazil_18_part4.json
-Britain_28_part4.json
-France_28_part4.json
-Germany_28_part4.json
-Indonesia_24_part4.json
-Japan_28_part4.json
-Jordan_28_part4.json
-Lebanon_28_part4.json
-Mexico_28_part4.json
-Nigeria_18_part4.json
-Pakistan_28_part4.json
-Russia_28_part4.json
-Turkey_28_part4.json
-)
+files=(Brazil_12_part3.json Brazil_14_part1.json Brazil_18_part4.json Brazil_20_part2.json 
+Britain_28_part1.json Britain_28_part2.json Britain_28_part3.json Britain_28_part4.json 
+France_28_part1.json France_28_part2.json France_28_part3.json France_28_part4.json 
+Germany_28_part1.json Germany_28_part2.json Germany_28_part3.json Germany_28_part4.json 
+Indonesia_18_part3.json Indonesia_22_part1.json Indonesia_24_part4.json Indonesia_28_part2.json 
+Japan_28_part1.json Japan_28_part2.json Japan_28_part3.json Japan_28_part4.json 
+Jordan_28_part1.json Jordan_28_part2.json Jordan_28_part3.json Jordan_28_part4.json 
+Lebanon_28_part1.json Lebanon_28_part2.json Lebanon_28_part3.json Lebanon_28_part4.json 
+Mexico_28_part1.json Mexico_28_part2.json Mexico_28_part3.json Mexico_28_part4.json 
+Nigeria_12_part3.json Nigeria_14_part1.json Nigeria_16_part2.json Nigeria_18_part4.json 
+Pakistan_28_part1.json Pakistan_28_part2.json Pakistan_28_part3.json Pakistan_28_part4.json 
+Russia_28_part1.json Russia_28_part2.json Russia_28_part3.json Russia_28_part4.json 
+Turkey_28_part1.json Turkey_28_part2.json Turkey_28_part3.json Turkey_28_part4.json)
+
 
 
 total=${#files[@]}
@@ -36,6 +35,7 @@ for f in "${files[@]}"; do
         --num_seed 8 \
         --batch_size "$batch_size" \
         --file_name "$f" \
+        --use_goldseed 1 \
         > /dev/null 2>&1
 
     # Print success or failure
